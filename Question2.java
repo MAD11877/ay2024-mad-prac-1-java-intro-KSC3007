a35b1dfb9ccfe4f523ded69ccc97dfd1633be5aa
Koh Shan Chun <150001309+KSC3007@users.noreply.github.com>
2024-04-15 15:16:12 +0800

    Adding code

--- Question2.java
+++ Question2.java
@@ -8,9 +8,14 @@ public class Question2
      * Prompts user for height and weight, calculate and print out the BMI of the user
      * 
      * Hint: in.nextDouble() to read double
-     */
+    */
      
     Scanner in = new Scanner(System.in);
+    System.out.print("Input weight in kilogram: ");
+    double weight = in.nextDouble();
+    System.out.print("Input height in meters: ");
     double height = in.nextDouble();
+    double BMI = weight / (height * height);
+    System.out.print("The Body Mass Index (BMI) is " + BMI + " kg/m2");
   }
 }
