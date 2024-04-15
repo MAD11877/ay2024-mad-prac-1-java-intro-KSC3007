54bbb9a3354737b76be5138d097c93aea27296d6
Koh Shan Chun <150001309+KSC3007@users.noreply.github.com>
2024-04-15 15:25:20 +0800

    Adding code

--- Question3.java
+++ Question3.java
@@ -10,7 +10,11 @@ public class Question3
      * Hint: in.nextInt() to read integer
      */
      
-    Scanner in = new Scanner(System.in);
+    Scanner in = new Scanner(System.in);Scanner in = new Scanner(System.in);
+    System.out.print("Input integer: ");
+    int input = in.nextInt();
+    int result = (input * input);
+    System.out.print("Result: " + result);
     
   }
 }
